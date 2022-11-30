526d5fc3aa4aad1b805a839c7e3301dbc3d6eea2
Rijawan Barudawale <rijawanbarudawale123@gmail.com>
2022-12-01 00:05:48 +0530

    prime and palindrome checked

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,11 @@ for(i=str.length-1;i>=0;i--)
 {
      bag=bag+str[i];
 }
+   if(bag==str)
+      {
+        console.log("palindrome");
+      }
+   else
+      {
+        console.log("not a prime");
+      }
